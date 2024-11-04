27c305790cb451ff547a8d5ee34836121a57ab80
mateopozor02 <mateopozoruiz02@hotmail.com>
2024-11-03 22:02:03 -0500

    Basic funcionality working (kind of)

--- Project3/TraderImpl.java
+++ Project3/TraderImpl.java
@@ -21,69 +21,61 @@ public class TraderImpl implements Trader {
 
     public void get(Order order) throws InterruptedException {
         // Keep checking each grain type until we can fulfill the order
-        for (Grain g : Grain.values()) {
-            while (stock.get(g) < order.get(g)) {
-                if (g == specialty) {
-                    // For our specialty grain, just wait for more deliveries
-                    synchronized(this) {
-                        wait();
+        for (Grain g : Grain.values()){
+            while (stock.get(g) < order.get(g)){
+                // If g is our specialty grain, just wait for more deliveries
+                if (g == specialty){
+                    synchronized(this){
+                            wait(); 
                     }
                 } else {
-                    // For other grains, try to swap if we have enough specialty grain
+                    // For another grain, try to swap if we have enough specialty grain
                     int amountNeeded = order.get(g) - stock.get(g);
                     int specialtyAvailable = stock.get(specialty);
                     
-                    if (specialtyAvailable >= amountNeeded) {
-                        // Try to atomically deduct our specialty grain
-                        boolean success = stock.replace(specialty, specialtyAvailable, 
-                                                     specialtyAvailable - amountNeeded);
-                        
-                        if (success) {
-                            try {
-                                P3.specialist(g).swap(specialty, amountNeeded);
-                            } catch (InterruptedException e) {
-                                // If swap fails, restore our specialty grain
-                                stock.compute(specialty, (k, v) -> v + amountNeeded);
-                                throw e;
-                            }
+                    if (specialtyAvailable >= amountNeeded){
+                        // Swap the specialty grain for the other grain with that trader
+                        P3.specialist(g).swap(specialty, amountNeeded);
+
+                        synchronized(this){
+                            // Once the swap is done, deduct the amount from the specialty grain and add it to the other grain
+                            stock.compute(specialty, (k, v) -> v - amountNeeded);
+                            stock.compute(g, (k, v) -> v + amountNeeded);
+                            System.out.println("Trader of " + specialty + "requested " + amountNeeded + " " + g + " and got it");
                         }
                     } else {
-                        // Not enough specialty grain to trade, wait for more
-                        synchronized(this) {
+                        // If we don't have enough specialty grain, wait for more deliveries
+                        synchronized(this){
                             wait();
                         }
                     }
                 }
             }
-            // Deduct the amount from our stock once we have enough
+            // If we have enough of the grain, deduct it from the stock
             stock.compute(g, (k, v) -> v - order.get(g));
         }
     }
 
-    public void swap(Grain what, int amt) throws InterruptedException {
-        // Try to perform the swap without full synchronization
-        while (true) {
-            int currentSpecialty = stock.get(specialty);
-            if (currentSpecialty >= amt) {
-                // Try to atomically deduct specialty grain
-                if (stock.replace(specialty, currentSpecialty, currentSpecialty - amt)) {
-                    // Add the received grain
-                    stock.compute(what, (k, v) -> v + amt);
-                    synchronized(this) {
-                        notifyAll(); // Wake up waiting threads
-                    }
-                    return;
-                }
-            } else {
-                synchronized(this) {
-                    wait();
-                }
-            }
+    public synchronized void swap(Grain what, int amt) throws InterruptedException {
+        System.out.println("Trader of " + specialty + " received a swap request for " + amt + " " + what);
+        // Keep checking until we can fulfill the swap
+        while (stock.get(specialty) < amt){
+            wait();
         }
+        // We have enough specialty grain, so we can swap
+        stock.compute(specialty, (k, v) -> v - amt);
+        stock.compute(what, (k, v) -> v + amt);   
+        
+        System.out.println("Stock after swap: " + stock);
+
+        //System.out.println("Trader of " + specialty + " swapped " + amt + " " + specialty + " for " + amt + " " + what);
     }
 
     public synchronized void deliver(int amt) throws InterruptedException {
+        System.out.println("Delivering " + amt + " " + specialty + " to the trader");
+        System.out.println("Stock before delivery: " + stock);
         stock.compute(specialty, (k, v) -> v + amt);
+        System.out.println("Stock after delivery: " + stock);
         notifyAll();
     }
 }
